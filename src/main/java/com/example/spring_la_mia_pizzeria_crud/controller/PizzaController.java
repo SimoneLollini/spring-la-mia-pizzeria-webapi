3cf81124de61859720fcec2f89b9ed433014cac3
SimoneLollini <lollinisimone@gmail.com>
2023-03-30 16:54:08 +0200

    add searchbar

--- src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -3,12 +3,14 @@ package com.example.spring_la_mia_pizzeria_crud.controller;
 import com.example.spring_la_mia_pizzeria_crud.model.Pizza;
 import com.example.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -21,8 +23,14 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model) {
-        List<Pizza> result = pizzaRepository.findAll();
+    public String index(Model model, @RequestParam(name = "q") Optional<String> keyword) {
+        List<Pizza> result;
+        if (keyword.isPresent()) {
+            result = pizzaRepository.findByNameContainingIgnoreCase(keyword.get());
+            model.addAttribute("keyword", keyword.get());
+        } else {
+            result = pizzaRepository.findAll(Sort.by("name"));
+        }
         model.addAttribute("pizzas", result);
         return "/pizzas/index";
     }
--- src/main/java/com/example/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -3,5 +3,12 @@ package com.example.spring_la_mia_pizzeria_crud.repository;
 import com.example.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+    //costruisce una query che richiami dal modello pizza un nome che contiene
+    // la String name che viene passata come parametro
+    // select name from pizza where name like "%stringa che viene passata%"
+    public List<Pizza> findByNameContainingIgnoreCase(String name);
 }
