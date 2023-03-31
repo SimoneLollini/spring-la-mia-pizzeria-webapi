f0d1bea6373545213dbe5db7794de66dae8bb620
SimoneLollini <lollinisimone@gmail.com>
2023-03-31 14:58:26 +0200

    add store method in PizzaController

--- src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -2,15 +2,14 @@ package com.example.spring_la_mia_pizzeria_crud.controller;
 
 import com.example.spring_la_mia_pizzeria_crud.model.Pizza;
 import com.example.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -44,5 +43,25 @@ public class PizzaController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza not found");
         }
+    }
+
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "/pizzas/create";
+
+    }
+
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors())
+            return "/pizzas/create";
+
+        pizzaRepository.save(formPizza);
+        return "redirect:/pizzas";
+
     }
 }
