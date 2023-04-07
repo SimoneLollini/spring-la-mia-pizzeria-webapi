4007514c3662fde751f936430dd65ecfd764a35d
SimoneLollini <lollinisimone@gmail.com>
2023-04-07 17:58:12 +0200

    Add security configuration

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,5 +15,12 @@ INSERT INTO db_pizzeria.ingredients(description, name)VALUES('descrizione ingred
 INSERT INTO db_pizzeria.ingredients(description, name)VALUES('descrizione ingrediente bella', 'ingrediente 3');
 INSERT INTO db_pizzeria.ingredients(description, name)VALUES('descrizione ingrediente bella', 'ingrediente 4');
 INSERT INTO db_pizzeria.ingredients(description, name)VALUES('descrizione ingrediente bella', 'ingrediente 5');
-INSERT INTO db_pizzeria.`user`(id, password, username)VALUES(1, '{noop}password', 'username');
+INSERT INTO db_pizzeria.`user`(id, password, username)VALUES(1, '{noop}password', 'admin');
+INSERT INTO db_pizzeria.`user`(id, password, username)VALUES(2, '{noop}password', 'user');
+
+INSERT INTO role (id, name) VALUES(1, 'ADMIN');
+INSERT INTO role (id, name) VALUES(2, 'USER');
+
+INSERT into user_roles(user_id, roles_id) VALUES(1, 1);
+INSERT into user_roles(user_id, roles_id) VALUES(2, 2);
 
